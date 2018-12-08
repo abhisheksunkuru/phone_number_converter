a472127a3a8bda16eae8fb44fd10afe320e08584
Abhishek <abhisheksunkuru@gmail.com>
2018-12-08 21:50:22 +0530

    find words for number

--- converter.rb
+++ converter.rb
@@ -1,5 +1,6 @@
 require_relative 'lib/converter/parser'
 require_relative 'lib/converter/conversion'
+#require_relative 'dictionary.txt'
 
 
 p Converter::Parser.new.parse(ARGV[0])
--- lib/converter/conversion.rb
+++ lib/converter/conversion.rb
@@ -14,8 +14,41 @@ module Converter
 
     }
 
+    DICTIONARY = File.readlines('dictionary.txt').map{|w| w.strip}
+
+    def self.valid_word?(word)
+      upcase_word = word.upcase
+      word  = DICTIONARY.bsearch{|w| w >= upcase_word}
+
+      word == upcase_word
+    end
+
+    def self.number_to_word(number)
+      result = []
+      possible_combinations = nil
+      number.each_char do |digit|
+
+        if possible_combinations
+          possible_combinations = possible_combinations.product(NUMBER_HASH[digit.to_i])
+        else
+          possible_combinations = NUMBER_HASH[digit.to_i]
+        end
+      end
+
+      test_words = possible_combinations.map { |e| e.flatten().join}
+
+      test_words.select! { |w| valid_word?(w) }
+
+      test_words
+    end
+
     def self.number_to_words(number)
-      binding.pry
+      # phone_number  = number.dup
+      # all_words = number_to_word(number)
+      # phone_number.each_char do |char|
+      #   words_list =
+
+      # ends
     end
   end
 end
